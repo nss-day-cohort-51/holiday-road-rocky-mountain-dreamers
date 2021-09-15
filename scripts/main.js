101fb1b7dc06f7790ced7ff55f64604eb48e4314
Jake Lashenik <jmlashen@gmail.com>
2021-09-15 13:47:30 -0500

    park dropdown and event listener for displaying park info in park preview

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,19 @@ import { eatList } from "./eateries/EateryList.js";
 import { getEateries } from "./eateries/EateryDataManager.js";
 import { bizList } from "./attractions/attractionList.js";
 import { getBizarreries } from "./attractions/AttractionDataManager.js";
-import { getParks } from "./parks/ParkDataManager.js";
+import { getParks, getSoloPark } from "./parks/ParkDataManager.js";
 import { statesList } from "./states/statesList.js";
 import { getStates } from "./states/statesDataManager.js";
+import { ParkList } from "./parks/ParkList.js";
+import { park } from "./parks/Park.js";
+import { parkPreview } from "./parks/parkPreview.js";
+
+
+
+
+
+
+
 
 
 const showEat = () => {
@@ -26,13 +36,23 @@ const showBiz = () => {
     }
     )
 }
-const showParks = (stateCode) => {
+const showPark = (stateCode) => {
     const parkElement = document.querySelector("#park");
     getParks(stateCode).then((allParks) => {
-        parkElement.innerHTML = getParksList(allParks);
+        parkElement.innerHTML = ParkList(allParks);
     })
 
 
+}
+
+const showParkPreview = (parkCode) => {
+    const parkPreviewElement = document.querySelector("#parkDisplay");
+    getSoloPark(parkCode).then((soloParkData) => {
+        console.log(soloParkData)
+        parkPreviewElement.innerHTML = parkPreview(soloParkData[0])
+    })
+
+    
 }
 
 
@@ -41,10 +61,18 @@ const showParks = (stateCode) => {
 const applicationElement = document.querySelector("#leftSection");
 
 applicationElement.addEventListener("change", event =>{
-    if (event.target.id === "#state") {
-    showParks(event.target.value)
+    
+    if (event.target.id === "state") {
+    console.log(event.target.value)
+        showPark(event.target.value)
+    }
+    else if (event.target.id=== "park"){
+        showParkPreview(event.target.value)
+        
     }
 })
+
+
 
 const showStates = () => {
     // This Function takes the imported info from attractions and adds it to the drop down menu in HTML
@@ -54,6 +82,15 @@ const showStates = () => {
     }
     )
 }
+
+// const applicationElement = document.querySelector("park")
+// documentElement.addEventListener("click", event =>{
+//     if (event.target.value) {
+//         console.log("drop down value", event.target.value)
+//     }
+// }
+
+
 
 
 
