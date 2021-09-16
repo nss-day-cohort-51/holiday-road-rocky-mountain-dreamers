eb601ef7553beb5caee947c9f9a4afb0ff328982
Chris Murphy <theworldisyours@gmail.com>
2021-09-16 11:49:46 -0400

    national parks list, value gathering, and save trip button

--- scripts/parks/parkPreview.js
+++ scripts/parks/parkPreview.js
@@ -2,25 +2,16 @@ export const parkPreview = (parkObject) => {
     return `
       <section>
       <div id="parkImage">
-      <img src=${parkObject.images[2].url}>
-      
-      
+      <img src=${parkObject.images[2] != undefined ? parkObject.images[2].url : "https://www.nps.gov/common/commonspot/templates/images/logos/nps_social_image_02.jpg"} alt="Image Missing">
+           
       <div class="parkImageDetail">
-      <h4>${parkObject.fullName}</h4>
+      <h4 id="parkImageFullName">${parkObject.fullName}</h4>
       <div class="parkImageDesc">
       <p>${parkObject.description}</p>
       <button id="parkDetailsButton">Details</button>
       </div>
       </div>
-      </div>
-      
-      
-      
-      
-      
-      
-      
-      
+      </div>      
       </section>
     `
 }
---
+++ scripts/trips/tripDataManager.js
@@ -0,0 +1,11 @@
+export const createTripEntry = tripObj => {
+    return fetch("http://localhost:8088/trips", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(tripObj)
+  
+    })
+        .then(response => response.json())
+  }
